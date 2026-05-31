5af0684a0f906100bb6494201d042ba450e216c0
Brandon Nichols <brandon@vyntechs.com>
2026-05-31 09:42:19 -0500

    feat(theme): render JuneSky for june; retire SunShimmer

--- components/system/index.ts
+++ components/system/index.ts
@@ -15,6 +15,8 @@ export { TVTimerArc } from "./TVTimerArc";
 export { QRBlock } from "./QRBlock";
 export { Weather, weatherLabel } from "./Weather";
 export { Lightning, fireLightningBeat } from "./Lightning";
+export { JuneSky, fireJuneBeat } from "./JuneSky";
+export type { JuneBeatKind } from "./JuneSky";
 export { ParticleField } from "./ParticleField";
 export { EmptyState } from "./EmptyState";
 export type { EmptyStateProps } from "./EmptyState";
